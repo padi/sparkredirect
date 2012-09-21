f3df00dca471fa4cf551e1a9cb83769328e5a62e
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-09-21 12:46:43 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sparkredirect::Application.config.secret_token = 'f6e75340ebe99802a4837bcfc976b35e4658dc3e29e1afb551e7953e77d0a06da0211c2ae1997e692764077f5297fc979a3d578bdb4cd0cd15edbcc231d021f0'
